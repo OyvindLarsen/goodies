ca6a96e8825a02d422871b83e523b46d62c710ed
Øyvind Larsen <larsen@goodies.no>
2016-10-26 14:35:45 +0200

    carousel nettside animasjon

--- index.php
+++ index.php
@@ -325,7 +325,37 @@ $error = "";
                 </div>
                 <div class="row">
                   <div class="">
-                    <div class="col-md-12 hidden right" id="nettAni2"><img id="image" src="img/grafikk/iMacBlog.png"></div>
+                    <div class="col-md-12 hidden right image no-padding" id="nettAni2">
+                    <img  src="img/grafikk/iMacBlog.png">
+                    <div class="carouselle">
+                      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                          <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                          </ol>
+                          <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                              <img src="img/grafikk/BlogNy.svg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="img/grafikk/OnePager.svg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="img/grafikk/BlogNy.svg" alt="Third slide">
+                            </div>
+                          </div>
+                          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                            <span class="icon-prev" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                            <span class="icon-next" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
--- scripts.js
+++ scripts.js
@@ -820,4 +820,3 @@ function canvas () {
 }
 
 */
-
